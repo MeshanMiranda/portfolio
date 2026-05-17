e96254a2f75c54dff334af90f1dd58b11aa09985
Meshan Miranda <prashansamm200327@gmail.com>
2026-05-17 17:33:43 +0530

    Modifed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     const targetId = e.currentTarget.getAttribute("href");
     const targetElement = document.querySelector(targetId);
     setActiveSection(targetId);
-    
+
     if (targetElement) {
       targetElement.scrollIntoView({ behavior: "smooth" });
 
@@ -157,10 +157,10 @@ const Navbar = () => {
             className="flex flex-col mt-4 space-y-2 text-base font-medium md:hidden"
           >
             <li><a href="#hero" onClick={handleMobileNavClick} className="text-blue-500 hover:underline">Home</a></li>
+            <li><a href="#experience" onClick={handleMobileNavClick} className="text-white hover:text-blue-500">Experience</a></li>
             <li><a href="#educations" onClick={handleMobileNavClick} className="text-white hover:text-blue-500">Educations</a></li>
             <li><a href="#technologies" onClick={handleMobileNavClick} className="text-white hover:text-blue-500">Skills</a></li>
             <li><a href="#projects" onClick={handleMobileNavClick} className="text-white hover:text-blue-500">Projects</a></li>
-            <li><a href="#experience" onClick={handleMobileNavClick} className="text-white hover:text-blue-500">Experience</a></li>
             <li><a href="#contact" onClick={handleMobileNavClick} className="text-white hover:text-blue-500">Contact</a></li>
           </motion.ul>
         )}
@@ -173,24 +173,34 @@ const Navbar = () => {
             <a
               href="#hero"
               onClick={handleNavClick}
-              className={`${
-                activeSection === "#hero"
+              className={`${activeSection === "#hero"
                   ? "text-blue-700 dark:text-blue-500"
                   : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
-              }`}
+                }`}
             >
               Home
             </a>
           </li>
           <li>
             <a
-              href="#educations"
+              href="#experience"
               onClick={handleNavClick}
-              className={`${
-                activeSection === "#educations"
+              className={`${activeSection === "#experience"
                   ? "text-blue-700 dark:text-blue-500"
                   : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
-              }`}
+                }`}
+            >
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#educations"
+              onClick={handleNavClick}
+              className={`${activeSection === "#educations"
+                  ? "text-blue-700 dark:text-blue-500"
+                  : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
+                }`}
             >
               Educations
             </a>
@@ -199,11 +209,10 @@ const Navbar = () => {
             <a
               href="#technologies"
               onClick={handleNavClick}
-              className={`${
-                activeSection === "#technologies"
+              className={`${activeSection === "#technologies"
                   ? "text-blue-700 dark:text-blue-500"
                   : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
-              }`}
+                }`}
             >
               Skills
             </a>
@@ -212,37 +221,22 @@ const Navbar = () => {
             <a
               href="#projects"
               onClick={handleNavClick}
-              className={`${
-                activeSection === "#projects"
+              className={`${activeSection === "#projects"
                   ? "text-blue-700 dark:text-blue-500"
                   : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
-              }`}
+                }`}
             >
               Projects
             </a>
           </li>
           <li>
             <a
-              href="#experience"
-              onClick={handleNavClick}
-              className={`${
-                activeSection === "#experience"
-                  ? "text-blue-700 dark:text-blue-500"
-                  : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
-              }`}
-            >
-              Experience
-            </a>
-          </li>
-          <li>
-            <a
               href="#contact"
               onClick={handleNavClick}
-              className={`${
-                activeSection === "#contact"
+              className={`${activeSection === "#contact"
                   ? "text-blue-700 dark:text-blue-500"
                   : "text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-500"
-              }`}
+                }`}
             >
               Contact
             </a>
