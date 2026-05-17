9be079e49ba1505b4ec12b2b0b2cb0ef62f43056
Meshan Miranda <prashansamm200327@gmail.com>
2026-05-17 17:02:45 +0530

    Added readme file and modify drive link

---
+++ github_readme.txt
@@ -0,0 +1,6 @@
+git status
+git add .
+git commit -m "Added genius soft"
+git push origin main
+
+npm run deploy
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from "framer-motion"
 import { useState } from "react";
 
 const containerVariants = {
-  hidden: {opacity: 0, x: -100},
+  hidden: { opacity: 0, x: -100 },
   visible: {
     opacity: 1,
     x: 0,
@@ -16,7 +16,7 @@ const containerVariants = {
 }
 
 const childVariants = {
-  hidden: {opacity: 0, x: -100},
+  hidden: { opacity: 0, x: -100 },
   visible: {
     opacity: 1,
     x: 0,
@@ -65,35 +65,35 @@ const Hero = () => {
     <div className="pt-[80px] md:pt-[60px]">
       <div className="flex flex-wrap lg:flex-row-reverse">
         <div className="w-full lg:w-1/2">
-            <div className="flex justify-center lg:p-8">
-                <motion.img src={profilePic} alt="Meshan Miranda" 
-                className="border border-stone-900 rounded-3xl" 
-                width={650}
-                height={650}
-                initial={{x: 100, opacity: 0}}
-                animate={{x: 0, opacity: 1}}
-                transition={{duration: 1, delay: 1.5}}
-                />
-            </div>
+          <div className="flex justify-center lg:p-8">
+            <motion.img src={profilePic} alt="Meshan Miranda"
+              className="border border-stone-900 rounded-3xl"
+              width={650}
+              height={650}
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
+            />
+          </div>
         </div>
         <div className="w-full lg:w-1/2">
-            <motion.div 
+          <motion.div
             initial="hidden"
             animate="visible"
             variants={containerVariants}
             className="flex flex-col items-center lg:items-start mt-10">
-                <motion.h2 variants={childVariants}className="pb-2 text-4xl tracking-tighter font-semibold lg:text-8xl">Meshan Miranda</motion.h2>
-                <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 
+            <motion.h2 variants={childVariants} className="pb-2 text-4xl tracking-tighter font-semibold lg:text-8xl">Meshan Miranda</motion.h2>
+            <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 
                 to-stone-600 bg-clip-text text-3xl tracking-tighter text-transparent">Software Engineer</motion.span>
-                <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"><ProjectDescription description={HERO_CONTENT} /></motion.p>
-                <motion.a variants={childVariants} href="https://drive.google.com/file/d/1lu51gqqM97acyP5aO_tKJzV42Aqx70WU/view?usp=sharing"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    download
-                    className="bg-white rounded-full p-4 text-sm
-                    text-stone-800 mb-10">Download Resume </motion.a> 
-                
-            </motion.div>
+            <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"><ProjectDescription description={HERO_CONTENT} /></motion.p>
+            <motion.a variants={childVariants} href="https://drive.google.com/file/d/1dY_3UBQ8JMJYRH6b_natjVPXpvUMv9gj/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+              className="bg-white rounded-full p-4 text-sm
+                    text-stone-800 mb-10">Download Resume </motion.a>
+
+          </motion.div>
         </div>
       </div>
     </div>
